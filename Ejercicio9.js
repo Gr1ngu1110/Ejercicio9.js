04d3a0c1694ad40d67872ca2275f7321b91f37bf
Juan Esteban Novillo Wagner <118315701+Gr1ngu1110@users.noreply.github.com>
2022-11-24 15:16:01 -0300

    Add files via upload

---
+++ Ejercicio9.js
@@ -0,0 +1,21 @@
+var pais="España";
+var continente="Europa";
+var antiguedad="2021";
+var pais_y_continente=pais+" "+continente;
+
+pais="Mexico";
+continente="Latinoamerica";
+
+console.log (pais, continente, antiguedad);
+alert (pais_y_continente);
+
+var pais="Argentina";
+var continente="America del Sur";
+var antiguedad="2022";
+var pais_y_continente = pais + "Argentina" + continente;
+
+pais="Mexico";
+continente="Latinoamerica";
+
+console.log (pais, continente, antiguedad);
+alert (pais_y_continente);
